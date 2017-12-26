a06b3387001ff53069e503ba0ff3e1be029029d4
frogtube <u.pradere@gmail.com>
2017-12-26 14:19:51 +0100

    Initial commit with first phpuinit test

---
+++ tests/AppBundle/Entity/DinausorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinausorTest extends TestCase
+{
+    public function testThatYourComputerWorks()
+    {
+        $this->assertTrue(false);
+    }
+
+}
